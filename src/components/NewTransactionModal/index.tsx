e066779047ab2cb82d7baf4c02b9105d22bb3a8f
feliciovcm <feliciovcm@gmail.com>
2021-03-20 22:08:49 -0300

    Update api requesto to mock data

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,10 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Container, SelectedTypeButton, TransactionTypeButton } from "./styles";
 import closeIMG from "../../assets/close.svg";
+import incomeIMG from "../../assets/income.svg";
+import outcomeIMG from "../../assets/outcome.svg";
+import React, { FormEvent, useState } from "react";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,6 +15,18 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit");
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = { title, value, category, type };
+
+    api.post('/transactions', data)
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -25,11 +41,57 @@ export function NewTransactionModal({
       >
         <img src={closeIMG} alt="close" />
       </button>
-      <Container>
+      <Container onSubmit={handleSubmit}>
         <h2>Cadastrar transação</h2>
-        <input placeholder="Título" />
-        <input type="number" placeholder="Valor" />
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Título"
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={(event) => {
+            setValue(Number(event.target.value));
+          }}
+        />
+
+        <TransactionTypeButton>
+          <SelectedTypeButton
+            type="button"
+            onClick={() => {
+              setType("deposit");
+            }}
+            isActive={type === "deposit"}
+            activeColor="green"
+          >
+            <img src={incomeIMG} alt="Entrada" />
+            <span>Entrada</span>
+          </SelectedTypeButton>
+
+          <SelectedTypeButton
+            type="button"
+            onClick={() => {
+              setType("withdraw");
+            }}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={outcomeIMG} alt="Saida" />
+            <span>Saida</span>
+          </SelectedTypeButton>
+        </TransactionTypeButton>
+
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => {
+            setCategory(event.target.value);
+          }}
+        />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,7 +1,8 @@
+import { darken, transparentize } from "polished";
 import styled from "styled-components";
 
 export const Container = styled.form`
-  h2{
+  h2 {
     color: var(--title);
     margin-bottom: 2rem;
     font-size: 1.5rem;
@@ -19,16 +20,14 @@ export const Container = styled.form`
 
     &::placeholder {
       color: var(--text);
-
     }
 
-    & + input{
+    & + input {
       margin-top: 1rem;
     }
-
   }
 
-  button[type="submit"]{
+  button[type="submit"] {
     width: 100%;
     padding: 0 1.5rem;
     height: 4rem;
@@ -43,10 +42,57 @@ export const Container = styled.form`
     transition: filter 0.2s;
 
     &:hover {
-    filter: brightness(0.8);
+      filter: brightness(0.8);
+    }
   }
-  }
-
-  
 `;
 
+export const TransactionTypeButton = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+`;
+
+interface SelectedTypeButtonProps {
+  isActive: boolean;
+  activeColor: "green" | "red";
+}
+
+const colors = {
+  green: "#33CC95",
+  red: "#E62E4D",
+};
+
+export const SelectedTypeButton = styled.button<SelectedTypeButtonProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : "transparent"};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.2, "#d7d7d7")};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--title);
+  }
+`;
